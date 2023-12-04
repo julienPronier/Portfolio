674c68e56944b39126b36d65b0edf2d3b527460e
Pronier Julien <julien_pronier@ens.univ-artois.fr>
2023-12-04 14:02:48 +0100

    Feur

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white ">Julien Pronier</h1>
+      <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-6xl dark:text-white">Bienvenue sur mon Portfolio en Ligne</h2>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Développeur basé à <big className="dark:text-white">Arras</big> en France.</p>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Je suis passionné par le <big className="dark:text-white">développement informatique</big> en tout genre.</p>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Mon langage de progarammation préferé est <big className="dark:text-white">Python</big>.</p>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Je suis actuellement en <big className="dark:text-white">3ème année de BUT Informatique</big> à l'IUT de Lens.</p>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Je suis à la recherche d'une <big className="dark:text-white"> alternance</big> pour l'année prochaune.</p>
+      <p className="mb-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">Je suis disponible à partir du 15 Aout 2024.</p>
+    
+      <div id="photo">
+        <img className="" src="moi.png" alt="Moi après 2 jour à la salle" />
+      </div>
+
+      <p>Compétences</p>
+
+      <div id="carousel">
+      </div>
+    </main>
+    
+  )
+}
